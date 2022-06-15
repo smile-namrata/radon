6b6ba30affd6511ce9f0959aad0300d6fa3f9cc6
smile-namrata <namrata.jaiswal102441@gmail.com>
2022-06-15 21:22:56 +0530

    .

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,39 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt=require("jsonwebtoken");
+const userModel=require("../models/userModel")
 
-    next()
+
+const authentication= function(req,res,next){
+    let token = req.headers["x-Auth-token"];
+    if(token){next()}
+    if (!token){ token = req.headers["x-auth-token"];
+          if(token)
+                {next()  
+    }
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+}
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+
+//module.exports.authentication=authentication
+
+
+const auth= function(req,res,next){
+// try{
+    let token = req.headers["x-auth-token"];
+    let decodedToken=jwt.verify(token,"functionup-radon");
+    //console.log(decodedToken)
+    let userToBeMOdified=req.params.userToBeMOdified
+    let userLoggedId=decodedToken.userId
+
+    if(userToBeMOdified!=userLoggedId) return res.send({status: false,msg:"user logged in tho llowed "})
     next()
+
+    // }catch(error){
+    //     res.status(404).snd(error);
+    // }
 }
+
+
+module.exports.authentication=authentication
+module.exports.auth=auth
