330409c24c4a547d509273278549764994029b1b
nickgautam <er.nishantgautam0011@gmail.com>
2022-07-18 16:22:43 +0530

    phase 1 of project4 with some validation

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -5,6 +5,20 @@ const shortid = require('shortid')
 const createUrl = async function(req,res){
     try{
         let data = req.body
+
+        if(Object.keys(data).length==0){
+            return res.status(400).send({
+                status: false, 
+                message: "Request body can't be empty"
+            })
+        }
+        var urlPattern = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/
+ 
+ 
+   if (!urlPattern.test(data.longUrl)){
+    return res.status(400).send({status: false , message: "Url is invalid"})
+   }
+ 
         let urlDecodedCode = shortid.generate(data)
         let shortDecodedUrl = "https://localhost:3000/"+urlDecodedCode
         data.shortUrl = shortDecodedUrl
@@ -13,19 +27,26 @@ const createUrl = async function(req,res){
         return res.status(201).send({status:true, data:result})
     }
     catch(err){
-        return res.status(500).send({status:false,msg:err.message})
+        return res.status(500).send({status:false,message:err.message})
     }
 
 }
-
+   
 const getUrl = async function(req,res){
 
     let code = req.params.urlCode
+    if(!code){
+        return res.status(400).send({status:false, message: "Please provide urlCode"})
+    }
     if(!shortid.isValid(code)){
         return res.status(400).send({status:false,msg:"Invalid Code"})
     }
     let getLongUrl = await urlModel.findOne({urlCode:code}).select({_id:0,longUrl:1})
-     res.status(200).send({status:true,data:getLongUrl})
+    if(!getLongUrl){
+        return res.status(404).send({status:false,message:"No Url Found"})
+    }
+
+    res.status(302).send("Redirect to " + getLongUrl.longUrl.toString())
 
 }
 
