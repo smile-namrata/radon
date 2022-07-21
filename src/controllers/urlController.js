aa2bfb6e671c5a53fa0a40ca07407f6561f1c0df
Mohd-Farhan-9990 <mohdfarhan9639@gmail.com>
2022-07-21 16:17:27 +0530

    Final

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -19,39 +19,59 @@ redisClient.on("connect",async function(){
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient)
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient)
+const SET_EX = promisify(redisClient.SETEX).bind(redisClient)
 
 const createUrl = async function(req,res){
     try{
-        let data = req.body
+            let data = req.body
 
-        if(Object.keys(data).length==0){
-            return res.status(400).send({
-                status: false, 
-                message: "Request body can't be empty"
-            })
+            if(Object.keys(data).length==0){
+                return res.status(400).send({
+                    status: false, 
+                    message: "Request body can't be empty"
+                })
+            }
+            var urlPattern = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/
+            if (!urlPattern.test(data.longUrl)){
+                return res.status(400).send({status: false , message: "Url is invalid"})
+            }
+
+            let cacheUrl = await GET_ASYNC(`${data.longUrl}`)
+            if(cacheUrl){
+                console.log("From cache")
+               return res.status(201).send({status:true,data:JSON.parse(cacheUrl)})
+            }
+            
+                    let dbData = await urlModel.findOne({longUrl:data.longUrl})
+                    if(dbData){
+                        let dbData1 = {
+                            urlCode:dbData.urlCode,
+                            longUrl:dbData.longUrl,
+                            shortUrl:dbData.shortUrl
+                        }
+                        console.log("from Db")
+                        await SET_EX(`${data.longUrl}`,120,JSON.stringify(dbData1))
+                       return res.status(201).send({status:true,data:dbData1})
+                    }
+                   
+                
+                let urlDecodedCode = shortid.generate().toLowerCase()
+                let shortDecodedUrl = "http://localhost:3000/"+urlDecodedCode
+                data.shortUrl = shortDecodedUrl
+                data['urlCode'] = urlDecodedCode
+                let result = await urlModel.create(data)
+                let res1 = {
+                    urlCode:result.urlCode,
+                    longUrl:result.longUrl,
+                    shortUrl:result.shortUrl
+                }
+                await SET_EX(`${data.longUrl}`,120,JSON.stringify(res1))
+                return res.status(201).send({status:true, data:res1})
+            
         }
-        var urlPattern = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/
- 
-        let longExist = await urlModel.findOne({longUrl:data.longUrl})
-        if(longExist){
-            return res.status(400).send({status: false , message: "Short Url is already generated with this longUrl"})
-        }
-
-   if (!urlPattern.test(data.longUrl)){
-    return res.status(400).send({status: false , message: "Url is invalid"})
-   }
-
-
-        let urlDecodedCode = shortid.generate().toLowerCase()
-        let shortDecodedUrl = "http://localhost:3000/"+urlDecodedCode
-        data.shortUrl = shortDecodedUrl
-        data['urlCode'] = urlDecodedCode
-        let result = await urlModel.create(data)
-        return res.status(201).send({status:true, data:result})
-    }
     catch(err){
-        return res.status(500).send({status:false,message:err.message})
-    }
+                return res.status(500).send({status:false,message:err.message})
+        }
 
 }
    
@@ -60,14 +80,14 @@ try{
         let urlCode = req.params.urlCode
         let cacheData = await GET_ASYNC(`${urlCode}`)
         if(cacheData){
-            res.status(302).redirect(JSON.parse(cacheData).longUrl)
+            res.status(302).redirect(cacheData)
         }
         else{
         let getLongUrl = await urlModel.findOne({urlCode:urlCode}).select({_id:0,longUrl:1})
         if(!getLongUrl){
             return res.status(404).send({status:false,message:"No Url Found"})
         }
-        await SET_ASYNC(`${urlCode}`, JSON.stringify(getLongUrl))
+        await SET_ASYNC(`${urlCode}`, getLongUrl.longUrl)
          res.status(302).redirect( getLongUrl.longUrl)
     }
     }
